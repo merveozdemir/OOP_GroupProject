cf6150988cc2cb6f0d1e5aca7f58ea0bc1d9bbdd
Merve Özdemir <32341236+merveozdemir@users.noreply.github.com>
2019-12-05 16:44:46 +0300

    init2

--- src/main/java/com/uniyaz/Calisan.java
+++ src/main/java/com/uniyaz/Calisan.java
@@ -1,4 +1,35 @@
 package com.uniyaz;
 
 public abstract class Calisan {
+    private String calistigiFirma;
+    private float maas;
+    private byte haftalikCalismaSaati;
+
+
+    abstract void isYap();
+    abstract float maasHesapla();
+
+    public String getCalistigiFirma() {
+        return calistigiFirma;
+    }
+
+    public void setCalistigiFirma(String calistigiFirma) {
+        this.calistigiFirma = calistigiFirma;
+    }
+
+    public float getMaas() {
+        return maas;
+    }
+
+    public void setMaas(float maas) {
+        this.maas = maas;
+    }
+
+    public byte getHaftalikCalismaSaati() {
+        return haftalikCalismaSaati;
+    }
+
+    public void setHaftalikCalismaSaati(byte haftalikCalismaSaati) {
+        this.haftalikCalismaSaati = haftalikCalismaSaati;
+    }
 }
---
+++ src/main/java/com/uniyaz/OfisteCalisabilme.java
@@ -0,0 +1,5 @@
+package com.uniyaz;
+
+public interface OfisteCalisabilme {
+
+}
